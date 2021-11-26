67fa59129bc7bf9acb0a976f3cc6f5cb0b4272f1
ernes <ernest.li@ontariotechu.net>
2021-11-25 22:39:41 -0500

    Added connection to heroku postges db

--- add_data.php
+++ add_data.php
@@ -9,7 +9,7 @@
     $SQL = "INSERT INTO yourdatabasename.data (date,temperature,humidity,pressure) VALUES ('$dateS','".$_GET["temp"]."','".$_GET["hum"]."','".$_GET["pr"]."')";     
 
     // Execute SQL statement
-    mysql_query($SQL);
+    pg_query($SQL);
 
     // Go to the review_data.php (optional)
     header("Location: index.php");
--- connect.php
+++ connect.php
@@ -1,9 +1,12 @@
 <?php
-$MyUsername = "root";  // enter your username for mysql
-$MyPassword = "uoit";  // enter your password for mysql
-$MyHostname = "localhost";      // this is usually "localhost" unless your database resides on a different server
-$MyDatabase = "greenhouse";
-$con = mysqli_connect($MyHostname , $MyUsername, $MyPassword, $MyDatabase);
+$url = parse_url("postgres://nhchgncqtdohmw:44f34a07049a26867f87219d2591a73ba66b665048b3c11b6e9b8e10269c476a@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d29h2tvqmjhdqj");
+$url["path"]=ltrim($url["path"],"/");
+$MyUsername = $url["user"];  // enter your username for mysql
+$MyPassword = $url["pass"];  // enter your password for mysql
+$MyHostname = $url["host"];      // this is usually "localhost" unless your database resides on a different server
+$MyDatabase = $url["path"];
+$MyPort = $url["port"];
+$con = pg_connect("host=$MyHostname dbname=$MyDatabase user=$MyUsername password=$MyPassword port=$MyPort");
 
 
 //$selected = mysqli_select_db($con, "greenhouse"); //Enter your database name here 
