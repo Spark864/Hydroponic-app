ac859367797c08bfb6463f94dffb9bb96667d58c
ernestPi <ernest.li@ontariotechu.net>
2021-11-26 19:52:36 -0500

    DHT11 sensor connects to heroku postgres db

--- DHT11Sensor.py
+++ DHT11Sensor.py
@@ -2,17 +2,19 @@ import adafruit_dht
 import time
 import board
 from datetime import datetime
-
-import mysql.connector
+import urllib.parse
+import psycopg2
 
 dhtDevice = adafruit_dht.DHT11(board.D4)
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="ernest123",
-    database="greenhouse"
+url = urllib.parse.urlparse("postgres://nhchgncqtdohmw:44f34a07049a26867f87219d2591a73ba66b665048b3c11b6e9b8e10269c476a@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d29h2tvqmjhdqj")
+mydb = psycopg2.connect(
+    host=url.hostname,
+    user=url.username,
+    password=url.password,
+    database=url.path[1:]
 )
 time.strftime("%d/%m/%Y %H:%M:%S")
+
 while True:
     try:
         humidity, temperature = dhtDevice.temperature, dhtDevice.humidity
@@ -22,9 +24,9 @@ while True:
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             print("date and time =", dt_string)
             mycursor = mydb.cursor()
-            sql ="INSERT INTO DataCollect (temperature, humidity, date) VALUES (%s, %s, %s)"
-            val=(temperature, humidity, now)
-            mycursor.execute(sql,val)
+            sql = "INSERT INTO DataCollect (temperature, humidity, date) VALUES (%s, %s, %s)"
+            val = (temperature, humidity, now)
+            mycursor.execute(sql, val)
             mydb.commit()
             print(mycursor.rowcount, "record inserted")
             mycursor.execute("SELECT * FROM DataCollect")
@@ -39,5 +41,3 @@ while True:
     except RuntimeError as e:
         print('Reading from DHT failure: ', e.args)
     time.sleep(5)
-    
-
