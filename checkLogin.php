62ec5586914d8d918f280958c28df2a9ffeef34b
minglaulomo <45078945+minglaulomo@users.noreply.github.com>
2021-11-27 19:02:08 -0500

    Update checkLogin.php

--- checkLogin.php
+++ checkLogin.php
@@ -2,7 +2,11 @@
     
     session_start();
     // Check if the user is already logged in, if yes then redirect him to index page
-    if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]){
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+        header("location: index.php");
+        exit;
+    }
+    else {
         header("location: login.php");
         exit;
     }
