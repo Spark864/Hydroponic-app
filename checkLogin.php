477a568cb96d16f0ad578fe09bc1728e46a7ff4d
ernes <ernest.li@ontariotechu.net>
2021-11-27 19:42:37 -0500

    fixed page by removing exit in check login

--- checkLogin.php
+++ checkLogin.php
@@ -3,8 +3,7 @@
     session_start();
     // Check if the user is already logged in, if yes then redirect him to index page
     if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-
-        exit;
+        
     }
     else {
         header("location: login.php");
