1cf2311e9bf7e3377d2c4bf4f935bdc8015c5a88
ernes <ernest.li@ontariotechu.net>
2021-12-01 14:26:14 -0500

    fixed and organized motar controller

--- WaterPumpRelaySwitch.py
+++ WaterPumpRelaySwitch.py
@@ -1,12 +1,11 @@
-
 from datetime import datetime
 from datetime import date
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setwarnings(False)    # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
-GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 
 # def time_in_range(start, end, x):
 #     """Return true if x is in the range [start, end]"""
@@ -21,7 +20,7 @@ GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and
 # print("Today date is: ", today.year)
 # print(type(today.month))
 
-# now = datetime.now()
+now = datetime.now()
 # print("now = ", now)
 # print(type(now))
 # print("now = ", now.hour)
@@ -29,33 +28,34 @@ GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and
 # print("now = ", now.year)
 # print(type(now.year))
 
-date1 = datetime(now.year,now.month,now.hour,10,0,0)
-date2 = datetime(now.year,now.month,now.hour,14,0,0)
-date3 = datetime(now.year,now.month,now.hour,16,0,0)
+date1 = datetime(now.year, now.month, now.hour, 10, 0, 0)
+date2 = datetime(now.year, now.month, now.hour, 14, 0, 0)
+date3 = datetime(now.year, now.month, now.hour, 16, 0, 0)
 
 isOn = False
-while(True):
-    if(now>=date1 and isOn is False):
+while True:
+    global isOn
+    if now >= date1 and isOn is False:
+        global isOn
         isOn = True
-        #turn on led
-        GPIO.output(13, GPIO.HIGH) # Turn on
-        sleep(60)                  # Sleep for 60 second
+        # turn on led
+        GPIO.output(13, GPIO.HIGH)  # Turn on
+        sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
         isOn = False
-        pass
-    elif(now>=date2 and isOn is False):
+    elif now >= date2 and isOn is False:
+        global isOn
         isOn = True
-        #turn on led
-        GPIO.output(13, GPIO.HIGH) # Turn on
-        sleep(60)                  # Sleep for 60 second
+        # turn on led
+        GPIO.output(13, GPIO.HIGH)  # Turn on
+        sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
         isOn = False
-        pass
-    elif(now>=date3 and isOn is False):
+    elif now >= date3 and isOn is False:
+        global isOn
         isOn = True
-        #turn on led
-        GPIO.output(13, GPIO.HIGH) # Turn on
-        sleep(60)                  # Sleep for 60 second
+        # turn on led
+        GPIO.output(13, GPIO.HIGH)  # Turn on
+        sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
         isOn = False
-        pass
