3cba1cf5ec06188ba972ff9cf1fb464f68c4b6b6
ernes <ernest.li@ontariotechu.net>
2021-12-01 14:53:08 -0500

    fixes on logic in water pump motar control

--- WaterPumpRelaySwitch.py
+++ WaterPumpRelaySwitch.py
@@ -1,18 +1,20 @@
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 from datetime import date
 import RPi.GPIO as GPIO
-from time import sleep
 
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 
-# def time_in_range(start, end, x):
-#     """Return true if x is in the range [start, end]"""
-#     if start <= end:
-#         return start <= x <= end
-#     else:
-#         return start <= x or x <= end
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
 
 # today = date.today()
 # print("Today date is: ", today)
@@ -28,34 +30,30 @@ now = datetime.now()
 # print("now = ", now.year)
 # print(type(now.year))
 
-date1 = datetime(now.year, now.month, now.date, 10, 0, 0)
-date2 = datetime(now.year, now.month, now.date, 14, 0, 0)
-date3 = datetime(now.year, now.month, now.date, 16, 0, 0)
+date1 = datetime(now.year, now.month, now.day, 10, 0, 0)
+date2 = datetime(now.year, now.month, now.day, 14, 0, 0)
+date3 = datetime(now.year, now.month, now.day, 16, 0, 0)
 
-isOn = False
 while True:
-    global isOn
-    if now >= date1 and isOn is False:
-        global isOn
-        isOn = True
+
+    if time_in_range(date1, date1 + timedelta(minutes=1), now):
+
         # turn on led
         GPIO.output(13, GPIO.HIGH)  # Turn on
-        sleep(60)  # Sleep for 60 second
+        time.sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
-        isOn = False
-    elif now >= date2 and isOn is False:
-        global isOn
-        isOn = True
+
+    elif time_in_range(date2, date2 + timedelta(minutes=1), now):
+
         # turn on led
         GPIO.output(13, GPIO.HIGH)  # Turn on
-        sleep(60)  # Sleep for 60 second
+        time.sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
-        isOn = False
-    elif now >= date3 and isOn is False:
-        global isOn
-        isOn = True
+
+    elif time_in_range(date3, date3 + timedelta(minutes=1), now):
+
         # turn on led
         GPIO.output(13, GPIO.HIGH)  # Turn on
-        sleep(60)  # Sleep for 60 second
+        time.sleep(60)  # Sleep for 60 second
         GPIO.output(13, GPIO.LOW)  # Turn off
-        isOn = False
+    time.sleep(5)
