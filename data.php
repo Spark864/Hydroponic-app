4872635f5284083a33f3592931688920ac807ee8
ernes <ernest.li@ontariotechu.net>
2022-03-14 03:10:54 -0400

    search bar not appearing fix

--- data.php
+++ data.php
@@ -270,7 +270,6 @@
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 
-<script src="AjaxEngineForTable.js" async=true></script>
 <script crossorigin="anonymous" integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ==" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
 <script src="GraphHourly.js"></script>
 <!-- AdminLTE for demo purposes -->
