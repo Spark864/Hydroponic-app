9ef22632555ad66156415adbd7a77c25ad4101de
ernes <ernest.li@ontariotechu.net>
2022-03-28 04:12:40 -0400

    testing

--- InterruptTest.py
+++ InterruptTest.py
@@ -11,11 +11,24 @@ def main():
             print("something")
             time.sleep(3)
     except:
+        print(threading.active_count())
+        print("All done!")
+        # perform any cleanup here
+
+
+def something_do():
+    try:
+        while not exit.is_set():
+            print("something")
+            time.sleep(3)
+    finally:
+        print(threading.active_count())
         print("All done!")
         # perform any cleanup here
 
 
 def raise_exception(self):
+    print(threading.active_count())
     thread_id = self.get_id()
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
                                                      ctypes.py_object(SystemExit))
@@ -28,6 +41,7 @@ def quit(signo, _frame):
     print("Interrupted by %d, shutting down" % signo)
     exit.set()
     raise_exception()
+    print(threading.active_count())
 
 
 if __name__ == '__main__':
@@ -37,4 +51,7 @@ if __name__ == '__main__':
     for sig in ('TERM', 'INT'):
         signal.signal(getattr(signal, 'SIG' + sig), quit)
 
-    threading.Thread(target=main())
+    t1 = threading.Thread(target=main())
+    t2 = threading.Thread(target=something_do())
+    raise_exception(t1)
+    raise_exception(t2)
