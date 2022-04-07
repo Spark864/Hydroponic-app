270e72956f44e61591559c6c2f8aa0ca725f79be
minglaulomo <45078945+minglaulomo@users.noreply.github.com>
2022-04-07 01:55:17 -0400

    Update GetDataForAjax.php

--- GetDataForAjax.php
+++ GetDataForAjax.php
@@ -1,7 +1,7 @@
 <?php                 
     include('connect.php'); 
                     //Retrieve all records and display them
-                    $result = pg_query($con, "Select * from datacollect order by id desc");
+                    $result = pg_query($con, "Select * from datacollect order by id");
                     $data = pg_fetch_all($result,PGSQL_ASSOC);
                     echo json_encode($data);
                     // Used for row color toggle
